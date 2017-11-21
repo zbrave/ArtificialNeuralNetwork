281f1bc4492cda5e627c44c01472fe290809ab40
Mert Aydar <mertaydar@outlook.com>
2017-11-21 15:36:15 +0300

    Init

---
+++ Assets/ControllerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour {
+
+    public float movSpeed;
+    public bool died;
+    public GameObject doNotColl;
+	// Use this for initialization
+	void Start () {
+        this.movSpeed = 3f;
+        this.died = false;
+
+        Physics.IgnoreCollision(gameObject.GetComponent<Collider>(), doNotColl.GetComponent<Collider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(transform.forward * movSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-transform.right * movSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(transform.right * movSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(-transform.forward * movSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag != "Terrain")
+            this.died = true;
+    }
+
+}
